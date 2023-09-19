ec6316e0644f3d3611a7e75ce098beaf8eb0e1da
Ben Field <fieldben97@gmail.com>
2023-09-19 14:51:33 +1000

    added in different colours for different highlights

--- frontend/contentScript.js
+++ frontend/contentScript.js
@@ -3,6 +3,7 @@ console.log('contentScript.js loaded');
 let currentHighlights = {};
 
 function highlightTexts(texts, type, color) {
+    console.log(type)
     if (currentHighlights[type]) {
         clearHighlights(type); // Clear existing highlights before setting new ones
         currentHighlights[type] = false;
@@ -16,6 +17,11 @@ function highlightTexts(texts, type, color) {
             });
         });
         currentHighlights[type] = true;
+
+        // Inject a style element to change the background color of the marks
+        const styleElement = document.createElement('style');
+        styleElement.textContent = `mark.${type} { background-color: ${color}; }`;
+        document.head.append(styleElement);
     }
 }
 
@@ -31,6 +37,7 @@ function addMessageListener() {
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         if (request.texts) {
             console.log('Received message:', request.texts);
+            console.log('Received type:', request.type);
             highlightTexts(request.texts, request.type, request.color); // Highlight new texts
             sendResponse({status: "success"}); 
         }
